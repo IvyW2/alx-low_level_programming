177709ad597ca01f0afcaf94bf2751c5e2ebae3e
IvyW2 <Ivywambugha2@gmail.com>
2022-06-02 18:09:13 -0700

    Commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -11,14 +11,14 @@ int main(void)
 	char cchar;
 	int iint;
 	long llong;
-	long long lllong;
+	long long ll_long;
 	float ffloat;
 
-	printf("size of a char: %zu byte(s)\n", sizeof(cchar));
-	printf("size of an int: %zu byte(s)\n", sizeof(iint));
-	printf("size of a long int: %zu byte(s)\n", sizeof(llong));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(lllong));
-	printf("size of a float: %zu byte(s)\n", sizeof(ffloat));
+	printf("Size of a char: %lu byte(s)\n", sizeof(cchar));
+	printf("Size of an int: %lu byte(s)\n", sizeof(iint));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(llong));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll_long));
+	printf("Size of a float: %lu byte(s)\n", sizeof(ffloat));
 
 	return (0);
 }
